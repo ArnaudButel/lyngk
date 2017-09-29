f319dde6460349e644f1a83ec3d30722ec04567c
BuildTools <unconfigured@null.spigotmc.org>
2017-09-29 15:50:19 +0200

    Test 2 qui fonctionne en utilisant la fonction isValide() de Corrdinates

--- test/test.js
+++ test/test.js
@@ -6,3 +6,19 @@ LyngkTestCase.prototype.test1 = function(){
     var coord1 = new Lyngk.Coordinates('A' , 1);
     assertFalse(coord1.isValide());
 }
+
+LyngkTestCase.prototype.test2 = function () {
+    var ligne = ['1','2','3','4','5','6','7','8','9'];
+    var col = ['A','B','C','D','E','F','G','H','I'];
+    var count = 0;
+    var coordATester;
+    for(var i in ligne) {
+        for(var j in col) {
+            coordATester = new Lyngk.Coordinates(col[j],ligne[i]);
+            if(coordATester.isValide()) {
+                count++;
+            }
+        }
+    }
+    assertEquals(count,43);
+}
