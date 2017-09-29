21a154f3a2018526d9e2f48897a5640465f2dca6
BuildTools <unconfigured@null.spigotmc.org>
2017-09-29 15:34:43 +0200

    Test 1 qui fonctionne avec la fonction isValide de Coordinate

--- test/test.js
+++ test/test.js
@@ -1,3 +1,8 @@
 'use strict';
 
 var LyngkTestCase = TestCase("LyngkTestCase");
+
+LyngkTestCase.prototype.test1 = function(){
+    var coord1 = new Lyngk.Coordinates('A' , 1);
+    assertFalse(coord1.isValide());
+}
